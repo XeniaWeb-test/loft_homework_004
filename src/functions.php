1657d61f1d2b94a0ba1d8c8fda1fd89899b5eca2
XeniaTkacheva <xenia1898@gmail.com>
2019-07-28 09:06:24 +0700

    Инициализация проекта ДЗ #4

---
+++ src/functions.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Задание #1
+ *
+ * @param string $fileName
+ */
+
+function task1($fileName)
+{
+
+}
+
+/**
+ * Задание #2
+ *
+ */
+
+function task2_1()
+{
+
+}
+
+/**
+ * Задание #3
+ *
+ */
+
+function task3()
+{
+
+}
+
+/**
+ * Задание #4
+ */
+
+function task4()
+{
+
+}
